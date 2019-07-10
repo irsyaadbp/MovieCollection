b6e3ed27ccda0a85fddcdbd59d4e2cd4c0cd8bd7
Irsyaad <irsyaadt@gmail.com>
2019-07-10 12:54:27 +0700

    Upload: -Migrate androidx -Add tab layout

--- app/src/main/java/com/irsyaad/dicodingsubmission/moviecollection/activity/MainActivity.java
+++ app/src/main/java/com/irsyaad/dicodingsubmission/moviecollection/activity/MainActivity.java
@@ -1,9 +1,16 @@
 package com.irsyaad.dicodingsubmission.moviecollection.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 
 import com.irsyaad.dicodingsubmission.moviecollection.R;
+import com.irsyaad.dicodingsubmission.moviecollection.fragment.FilmFragment;
+import com.irsyaad.dicodingsubmission.moviecollection.fragment.TvShowFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +18,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setFragment();
+    }
+
+    private void setFragment(){
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), FragmentPagerItems.with(this)
+                .add(R.string.film_title, FilmFragment.class)
+                .add(R.string.tvshow_title, TvShowFragment.class)
+                .create());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
+        viewPagerTab.setViewPager(viewPager);
     }
 }
