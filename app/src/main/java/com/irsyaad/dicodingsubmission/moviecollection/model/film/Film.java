069d469b9306588d33eb0fab957fafca8588ede0
Irsyaad Budi <irsyaadt@gmail.com>
2019-07-10 22:25:10 +0700

    Upload: -add data film -model

--- app/src/main/java/com/irsyaad/dicodingsubmission/moviecollection/model/film/Film.java
+++ app/src/main/java/com/irsyaad/dicodingsubmission/moviecollection/model/film/Film.java
@@ -1,4 +1,157 @@
 package com.irsyaad.dicodingsubmission.moviecollection.model.film;
 
-public class Film {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Film implements Parcelable {
+    private String title, overview, poster, genres, language, runtime;
+    private String budget, revenue, rating, director, star, year;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+
+    public String getBudget() {
+        return budget;
+    }
+
+    public void setBudget(String budget) {
+        this.budget = budget;
+    }
+
+    public String getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(String revenue) {
+        this.revenue = revenue;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getStar() {
+        return star;
+    }
+
+    public void setStar(String star) {
+        this.star = star;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.title);
+        dest.writeString(this.overview);
+        dest.writeString(this.poster);
+        dest.writeString(this.genres);
+        dest.writeString(this.language);
+        dest.writeString(this.runtime);
+        dest.writeString(this.budget);
+        dest.writeString(this.revenue);
+        dest.writeString(this.rating);
+        dest.writeString(this.director);
+        dest.writeString(this.star);
+        dest.writeString(this.year);
+    }
+
+    public Film() {
+    }
+
+    protected Film(Parcel in) {
+        this.title = in.readString();
+        this.overview = in.readString();
+        this.poster = in.readString();
+        this.genres = in.readString();
+        this.language = in.readString();
+        this.runtime = in.readString();
+        this.budget = in.readString();
+        this.revenue = in.readString();
+        this.rating = in.readString();
+        this.director = in.readString();
+        this.star = in.readString();
+        this.year = in.readString();
+    }
+
+    public static final Parcelable.Creator<Film> CREATOR = new Parcelable.Creator<Film>() {
+        @Override
+        public Film createFromParcel(Parcel source) {
+            return new Film(source);
+        }
+
+        @Override
+        public Film[] newArray(int size) {
+            return new Film[size];
+        }
+    };
 }
--- app/src/main/java/com/irsyaad/dicodingsubmission/moviecollection/model/film/FilmData.java
+++ app/src/main/java/com/irsyaad/dicodingsubmission/moviecollection/model/film/FilmData.java
@@ -1,4 +1,41 @@
 package com.irsyaad.dicodingsubmission.moviecollection.model.film;
 
+import java.util.ArrayList;
+
 public class FilmData {
+    private static String[][] data = new String[][]{
+            {"Avengers: Infinity War","As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.","https://image.tmdb.org/t/p/w600_and_h900_bestv2/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg","ADVENTURE, ACTION, FANTASY","English","2h 29m","$300,000,000.00","$2,046,239,637.00","8.5","2018","Anthony Russo, Joe Russo","Robert Downey Jr., Chris Hemsworth"},
+            {"Spider-Man: Into the Spider-Verse","Miles Morales is juggling his life between being a high school student and being a spider-man. When Wilson \"Kingpin\" Fisk uses a super collider, others from across the Spider-Verse are transported to this dimension.","https://image.tmdb.org/t/p/w600_and_h900_bestv2/iiZZdoQBEYBv6id8su7ImL0oCbD.jpg","ACTION, ADVENTURE, ANIMATION, SCIENCE FICTION, COMEDY","English","1h 57m","$90,000,000.00","$375,450,417.00","8.5","2018","Bob Persichetti, Peter Ramsey","Shameik Moore, Jake Johnson"},
+            {"A Star Is Born","Seasoned musician Jackson Maine discovers — and falls in love with — struggling artist Ally. She has just about given up on her dream to make it big as a singer — until Jack coaxes her into the spotlight. But even as Ally's career takes off, the personal side of their relationship is breaking down, as Jack fights an ongoing battle with his own internal demons.","https://image.tmdb.org/t/p/w600_and_h900_bestv2/wrFpXMNBRj2PBiN4Z5kix51XaIZ.jpg","DRAMA, ROMANCE, MUSIC","English","2h 15m","$36,000,000.00","$433,888,866.00","7.7","2018","Bradley Cooper","Lady Gaga, Bradley Cooper"},
+            {"Bohemian Rhapsody","Singer Freddie Mercury, guitarist Brian May, drummer Roger Taylor and bass guitarist John Deacon take the music world by storm when they form the rock 'n' roll band Queen in 1970. Hit songs become instant classics. When Mercury's increasingly wild lifestyle starts to spiral out of control, Queen soon faces its greatest challenge yet – finding a way to keep the band together amid the success and excess.","https://image.tmdb.org/t/p/w600_and_h900_bestv2/lHu1wtNaczFPGFDTrjCSzeLPTKN.jpg","DRAMA, MUSIC","English","2h 15m","$52,000,000.00","$835,137,710.00","8.0","2018","Bryan Singer","Rami Malek, Lucy Boynton"},
+            {"Incredibles 2","Elastigirl springs into action to save the day, while Mr. Incredible faces his greatest challenge yet – taking care of the problems of his three children.","https://image.tmdb.org/t/p/w600_and_h900_bestv2/9lFKBtaVIhP7E2Pk0IY1CwTKTMZ.jpg","ACTION, ADVENTURE, ANIMATION, FAMILY","English","1h 58m","$200,000,000.00","$1,241,891,456.00","7.7","2018","Brad Bird","Craig T. Nelson, Holly Hunter"},
+            {"Spider-Man: Far from Home","Peter Parker and his friends go on a summer trip to Europe. However, they will hardly be able to rest - Peter will have to agree to help Nick Fury uncover the mystery of creatures that cause natural disasters and destruction throughout the continent.","https://image.tmdb.org/t/p/w600_and_h900_bestv2/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg","ACTION, ADVENTURE,SCIENCE FICTION","English","2h 9m","$160,000,000.00","$580,055,628.00","7.7","2019","Jon Watts","Tom Holland, Samuel L. Jackson"},
+            {"Toy Story 4","Woody has always been confident about his place in the world and that his priority is taking care of his kid, whether that's Andy or Bonnie. But when Bonnie adds a reluctant new toy called \"Forky\" to her room, a road trip adventure alongside old and new friends will show Woody how big the world can be for a toy.","https://image.tmdb.org/t/p/w600_and_h900_bestv2/w9kR8qbmQ01HwnvK4alvnQ2ca0L.jpg","ADVENTURE, ANIMATION, COMEDY","English","1h 40m","$175,000,000.00","$519,802,847.00","8.3","2019","Josh Cooley","Tom Hanks, Tim Allen"},
+            {"John Wick: Chapter 3 – Parabellum","Super-assassin John Wick returns with a $14 million price tag on his head and an army of bounty-hunting killers on his trail. After killing a member of the shadowy international assassin’s guild, the High Table, John Wick is excommunicado, but the world’s most ruthless hit men and women await his every turn.","https://image.tmdb.org/t/p/w600_and_h900_bestv2/w9kR8qbmQ01HwnvK4alvnQ2ca0L.jpg","CRIME, ACTION, THRILLER","English","2h 11m","$55,000,000.00","$293,061,998.00","8.0","2019","Chad Stahelski","Keanu Reeves, Halle Berry"},
+            {"Aladdin","A kindhearted street urchin named Aladdin embarks on a magical adventure after finding a lamp that releases a wisecracking genie while a power-hungry Grand Vizier vies for the same lamp that has the power to make their deepest wishes come true.","https://image.tmdb.org/t/p/w600_and_h900_bestv2/3iYQTLGoy7QnjcUYRJy4YrAgGvp.jpg","ADVENTURE, FANTASY, ROMANCE","English","2h 10m","$183,000,000.00","$735,831,017.00","7.4","2019","Guy Ritchie","Will Smith, Mena Massoud"}
+    };
+
+    public static ArrayList<Film> getListData(){
+        ArrayList<Film> list = new ArrayList<>();
+
+        for (String[] aData : data){
+            Film film = new Film();
+
+            film.setTitle(aData[0]);
+            film.setOverview(aData[1]);
+            film.setPoster(aData[2]);
+            film.setGenres(aData[3]);
+            film.setLanguage(aData[4]);
+            film.setRuntime(aData[5]);
+            film.setBudget(aData[6]);
+            film.setRevenue(aData[7]);
+            film.setRating(aData[8]);
+            film.setYear(aData[9]);
+            film.setDirector(aData[10]);
+            film.setStar(aData[11]);
+
+            list.add(film);
+        }
+        return list;
+    }
 }
