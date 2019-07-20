050cc67637ee0c19a461ca3330aeb429980026fd
Irsyaad <irsyaadt@gmail.com>
2019-07-20 11:09:40 +0700

    Upload: - collapsing toolbar detail activity

--- app/src/main/java/com/irsyaad/dicodingsubmission/moviecollection/activity/DetailActivity.java
+++ app/src/main/java/com/irsyaad/dicodingsubmission/moviecollection/activity/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.irsyaad.dicodingsubmission.moviecollection.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.irsyaad.dicodingsubmission.moviecollection.R;
 import com.irsyaad.dicodingsubmission.moviecollection.model.film.Film;
 
@@ -18,5 +21,16 @@ public class DetailActivity extends AppCompatActivity {
 
         Film film = getIntent().getParcelableExtra(EXTRA_FILM);
         Toast.makeText(this, "kamu klik "+film.getTitle(), Toast.LENGTH_SHORT).show();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsingToolbar);
+        collapsingToolbarLayout.setTitle(film.getTitle());
+
+        collapsingToolbarLayout.setCollapsedTitleTextColor(
+                ContextCompat.getColor(this, R.color.colorWhite));
+        collapsingToolbarLayout.setExpandedTitleColor(
+                ContextCompat.getColor(this, R.color.colorPrimary));
     }
 }
